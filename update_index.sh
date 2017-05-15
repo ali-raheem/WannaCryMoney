26c84d479902caf670e453039d4cb2651c37a3fa
Ali Raheem <ali.raheem@gmail.com>
2017-05-15 15:40:35 +0100

    Added search to local currency

--- update_index.sh
+++ update_index.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed template.htm -e "s/__TOTALBTC/$(bash track_monies.sh)/" > temp1
+sed template.htm -e "s/__TOTALBTC/$(bash track_monies.sh)/g" > temp1
 sed temp1 -e "s/__DATETIME/$(date)/" > index.htm
 echo $(markdown *.md)/ >> index.htm
 echo "</body></html>" >> index.htm
