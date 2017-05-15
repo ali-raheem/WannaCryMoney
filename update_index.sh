a460a3b7df09271f580ceccfe0c9928b315e64c8
Ali Raheem <ali.raheem@gmail.com>
2017-05-15 15:50:16 +0100

    Fixed error in build

--- update_index.sh
+++ update_index.sh
@@ -2,6 +2,7 @@
 
 sed template.htm -e "s/__TOTALBTC/$(bash track_monies.sh)/g" > temp1
 sed temp1 -e "s/__DATETIME/$(date)/" > index.htm
-echo $(markdown *.md)/ >> index.htm
+markdown  Wannacrypt0r-FACTSHEET.md > temp2
+cat temp2 >> index.htm
 echo "</body></html>" >> index.htm
 rm temp1
