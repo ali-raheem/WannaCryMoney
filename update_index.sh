1bb71aa522a5f35eb854abe7e86213d51443a234
Ali Raheem <ali.raheem@gmail.com>
2017-05-15 15:37:47 +0100

    Added factsheet

--- update_index.sh
+++ update_index.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sed template.htm -e "s/__TOTALBTC/$(bash track_monies.sh)/" > index.htm
+sed template.htm -e "s/__TOTALBTC/$(bash track_monies.sh)/" > temp1
+sed temp1 -e "s/__DATETIME/$(date)/" > index.htm
+echo $(markdown *.md)/ >> index.htm
+echo "</body></html>" >> index.htm
+rm temp1
